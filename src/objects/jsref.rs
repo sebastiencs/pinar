92a2fb6f347aa6e74f3afe7ae5b41db4e621756b
Sebastien Chapuis <sebastien@chapu.is>
2019-09-18 11:41:01 +0800

    implement JsRef

--- src/objects/jsref.rs
+++ src/objects/jsref.rs
@@ -1,13 +1,19 @@
 
 use std::marker::PhantomData;
+use std::rc::Rc;
+
 use napi_sys::*;
 use crate::Result;
 use crate::prelude::*;
 
-#[allow(dead_code)]
-pub struct JsRef<T: JsValue> {
+pub(crate) struct JsRefInner {
     pub(crate) env: Env,
     pub(crate) js_ref: napi_ref,
+}
+
+#[derive(Clone)]
+pub struct JsRef<T: JsValue> {
+    pub(crate) inner: Rc<JsRefInner>,
     phantom: PhantomData<T>
 }
 
@@ -15,22 +21,109 @@ pub trait AsJsRef<T: JsValue> {
     fn as_js_ref(&self) -> Result<JsRef<T>>;
 }
 
-impl<T: JsValue> AsJsRef<T> for T {
-    fn as_js_ref(&self) -> Result<JsRef<T>> {
-        let env = self.get_value().env;
-        let mut js_ref: napi_ref = std::ptr::null_mut();
-        unsafe {
-            Status::result(napi_create_reference(
-                env.env(),
-                self.get_value().value,
-                1,
-                &mut js_ref as *mut napi_ref
-            ))?;
+impl<T> Drop for JsRef<T>
+where
+    T: JsValue
+{
+    fn drop(&mut self) {
+        if Rc::strong_count(&self.inner) > 1 {
+            return;
         }
-        Ok(JsRef {
-            env,
-            js_ref,
-            phantom: PhantomData
-        })
+        let inner = &self.inner;
+        unsafe {
+            Status::result(napi_delete_reference(
+                inner.env.env(),
+                inner.js_ref
+            )).expect("Fail to drop a JsRef");
+        }
     }
 }
+
+
+macro_rules! impl_jsref {
+    (
+        $( $jstype:ident ),*
+    ) => {
+        $(
+
+            impl<'e> AsJsRef<$jstype<'static>> for $jstype<'e>
+            {
+                fn as_js_ref(&self) -> Result<JsRef<$jstype<'static>>> {
+                    let env = self.get_value().env;
+                    let mut js_ref: napi_ref = std::ptr::null_mut();
+                    unsafe {
+                        Status::result(napi_create_reference(
+                            env.env(),
+                            self.get_value().value,
+                            1,
+                            &mut js_ref as *mut napi_ref
+                        ))?;
+                    }
+                    Ok(JsRef {
+                        inner: Rc::new(JsRefInner {
+                            env,
+                            js_ref,
+                        }),
+                        phantom: PhantomData
+                    })
+                }
+            }
+
+            impl<'a, 'e> JsRef<$jstype<'a>> {
+                pub fn get(&self) -> Result<$jstype<'e>> {
+                    let mut result = Value::new(self.inner.env);
+                    unsafe {
+                        Status::result(napi_get_reference_value(
+                            self.inner.env.env(),
+                            self.inner.js_ref,
+                            result.get_mut()
+                        ))?;
+                    }
+                    Ok($jstype::from(result))
+                }
+            }
+
+            impl<'e> JsValue for &$jstype<'e> {
+                #[inline]
+                fn get_value(&self) -> Value {
+                    self.value
+                }
+            }
+
+            impl<'e> JsValue for $jstype<'e> {
+                #[inline]
+                fn get_value(&self) -> Value {
+                    self.value
+                }
+            }
+
+            impl<'e> $jstype<'e> {
+                #[doc(hidden)]
+                #[inline]
+                pub fn from(value: Value) -> Self {
+                    Self { value, phantom: PhantomData }
+                }
+
+                #[inline]
+                pub(crate) fn clone(&self) -> Self {
+                    Self { value: self.value, phantom: PhantomData }
+                }
+            }
+
+        )*
+    }
+}
+
+impl_jsref!(
+    JsString,
+    JsObject,
+    JsArray,
+    JsNumber,
+    JsSymbol,
+    JsUndefined,
+    JsFunction,
+    JsExternal,
+    JsNull,
+    JsBoolean,
+    JsBigInt
+);
